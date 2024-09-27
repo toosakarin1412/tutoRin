aa3ae0b2e7fef996a79e0f47b25351904a8fbd05
toosakarin1412 <toosakarin1412@gmail.com>
2024-09-28 01:56:32 +0700

    feat: Materi Docker

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Tutor-Rin</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/toosakarin1412/tutoRin',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/toosakarin1412/tutoRin',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'tutor-rin by Diky Wahyudi <toosakarin1412>',
   },
 }
 
