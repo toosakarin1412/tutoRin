6a68eaea109305498052bac73967455f83189aa5
toosakarin1412 <toosakarin1412@gmail.com>
2024-09-28 12:47:03 +0700

    feat: about and other setting

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Tutor-Rin</span>,
+  logo: (
+    <>
+      <img src="https://github.com/toosakarin1412.png" alt="rin-profile" width={32}/>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Tutor-Rin
+      </span>
+    </>
+  ),
   project: {
     link: 'https://github.com/toosakarin1412/tutoRin',
   },
